e83e7918db54165ec310dd5db3e26e1583d48124
mindc1jun94 <mindc1jun94@c1jun77.iiht.tech>
2022-08-12 17:45:49 +0530

    College-Ms4

---
+++ college/src/main/java/com/mindtree/college/dto/Response.java
@@ -0,0 +1,17 @@
+package com.mindtree.college.dto;
+
+import com.mindtree.college.entity.College;
+
+public class Response {
+	
+	private CollegeResponse collegeData;
+
+	public CollegeResponse getCollegeData() {
+		return collegeData;
+	}
+
+	public void setCollegeData(CollegeResponse collegeData) {
+		this.collegeData = collegeData;
+	}
+
+}
